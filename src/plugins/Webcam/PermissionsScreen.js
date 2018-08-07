de222b59935de52d8ce33af4991ded117a749ca1
Mateusz Woskowicz <mateusz.woskowicz@ragnarson.com>
2018-08-07 19:51:53 +0200

    hejo

--- src/plugins/Webcam/PermissionsScreen.js
+++ src/plugins/Webcam/PermissionsScreen.js
@@ -3,9 +3,8 @@ const { h } = require('preact')
 module.exports = (props) => {
   return (
     <div class="uppy-Webcam-permissons">
-      <h1 class="uppy-Webcam-Title">Please allow access to your camera</h1>
-      <p>You have been prompted to allow camera access from this site.<br />
-      In order to take pictures with your camera you must approve this request.</p>
+      <h1 class="uppy-Webcam-Title">Please allow</h1>
+      <p>You have beenbbbbb</p>
     </div>
   )
 }
